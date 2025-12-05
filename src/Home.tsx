98423fe45cc57554a71c3673057c91d51d46b566
Mohannad-Algaali <hannodi.di@gmail.com>
2025-12-05 20:07:27 +0800

    fix navigation

--- src/Home.tsx
+++ src/Home.tsx
@@ -10,91 +10,92 @@ import Experience from "./componenets/Experience";
 import Projects from "./componenets/Projects";
 import Skills from "./componenets/Skills";
 import Footer from "./componenets/Footer";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  //@ts-ignore
-  const { t, i18n } = useTranslation();
-  const [isSettingsMenuOpen, setIsSettingsMenuOpen] = useState(false);
+	//@ts-ignore
+	const { t, i18n } = useTranslation();
+	const [isSettingsMenuOpen, setIsSettingsMenuOpen] = useState(false);
+	const navigate = useNavigate()
+	const iconSize = 35;
 
-  const iconSize = 35;
+	const goToPage = (pageUrl: string) => {
+		navigate(pageUrl)
+	};
 
-  const goToPage = (pageUrl: string) => {
-    window.location.assign(pageUrl);
-  };
+	const dockItems: DockItemData[] = [
+		{
+			icon: <FaHome size={iconSize} />,
+			label: t("home"),
+			onClick: () => {
+				// goToSection("/");
+			},
+		},
+		{
+			icon: <FaCode size={iconSize} />,
+			label: t("projects"),
+			onClick: () => {
+				goToPage("/projects");
+			},
+		},
+		{
+			icon: <IoMdSettings size={iconSize} />,
+			label: t("settings"),
+			onClick: () => {
+				setIsSettingsMenuOpen(!isSettingsMenuOpen);
+			},
+		},
+	];
 
-  const dockItems: DockItemData[] = [
-    {
-      icon: <FaHome size={iconSize} />,
-      label:t( "home"),
-      onClick: () => {
-        // goToSection("/");
-      },
-    },
-    {
-      icon: <FaCode size={iconSize} />,
-      label:t( "projects"),
-      onClick: () => {
-        goToPage("/projects");
-      },
-    },
-    {
-      icon: <IoMdSettings size={iconSize} />,
-      label:t( "settings"),
-      onClick: () => {
-        setIsSettingsMenuOpen(!isSettingsMenuOpen);
-      },
-    },
-  ];
+	const [theme, setTheme] = useState(
+		localStorage.getItem("data-theme") || "dark"
+	);
 
-  const [theme, setTheme] = useState(
-    localStorage.getItem("data-theme") || "dark"
-  );
+	useEffect(() => {
+		localStorage.setItem("data-theme", theme);
+		document.documentElement.setAttribute("data-theme", theme);
+	}, [theme]);
 
-  useEffect(() => {
-    localStorage.setItem("data-theme", theme);
-    document.documentElement.setAttribute("data-theme", theme);
-  }, [theme]);
+	const handleLanguage = () => {
+		if (i18n.language === "ar") {
+			i18n.changeLanguage("en");
+		} else {
+			i18n.changeLanguage("ar");
+		}
+	};
 
-  const handleLanguage = () => {
-    if (i18n.language === "ar") {
-      i18n.changeLanguage("en");
-    } else {
-      i18n.changeLanguage("ar");
-    }
-  };
+	const handleChangeTheme = () => {
+		if (theme === "dark") {
+			setTheme("light");
+		} else {
+			setTheme("dark");
+		}
+	};
 
-  const handleChangeTheme = () => {
-    if (theme === "dark") {
-      setTheme("light");
-    } else {
-      setTheme("dark");
-    }
-  };
+	return (
+		<div className="w-full sm:w-[40rem] m-auto p-5">
+			{isSettingsMenuOpen && (
+				<SettingsMenu
+					theme={theme}
+					handleChangeTheme={handleChangeTheme}
+					handleLanguage={handleLanguage}
+					onClose={() => setIsSettingsMenuOpen(false)}
+				/>
+			)}
 
-  return (
-    <div className="w-full sm:w-[40rem] m-auto p-5">
-      {isSettingsMenuOpen && (
-        <SettingsMenu
-          theme={theme}
-          handleChangeTheme={handleChangeTheme}
-          handleLanguage={handleLanguage}
-          onClose={() => setIsSettingsMenuOpen(false)}
-        />
-      )}
-
-      <div id="display" className="min-h-[100dvh] w-full ">
-        <OverView></OverView>
-        <About></About>
-        <Experience></Experience>
-        <Skills></Skills>
-        <Projects numProjects={3}></Projects>
-        <Footer></Footer>
-        <div
-          className={`fixed bottom-5 left-1/2 z-40 flex -translate-x-1/2 flex-col items-center`}
-        >
-          <Dock items={dockItems} theme={theme}></Dock>
-        </div>
-      </div>
-    </div>
-  );
+			<div id="display" className="min-h-[100dvh] w-full ">
+				<OverView></OverView>
+				<About></About>
+				<Experience></Experience>
+				<Skills></Skills>
+				<Projects numProjects={3}></Projects>
+				<Footer></Footer>
+				<div
+					className={`fixed bottom-5 left-1/2 z-40 flex -translate-x-1/2 flex-col items-center`}
+				>
+					<Dock items={dockItems} theme={theme}></Dock>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/ProjectsPage.tsx
+++ src/ProjectsPage.tsx
@@ -7,93 +7,91 @@ import Projects from "./componenets/Projects";
 import Footer from "./componenets/Footer";
 import type { DockItemData } from "./componenets/Dock";
 import Dock from "./componenets/Dock";
+import { useNavigate} from "react-router-dom";
 
 export default function ProjectsPage() {
-  //@ts-ignore
-  const { t, i18n } = useTranslation();
-  const [isSettingsMenuOpen, setIsSettingsMenuOpen] = useState(false);
+	//@ts-ignore
+	const { t, i18n } = useTranslation();
+	const [isSettingsMenuOpen, setIsSettingsMenuOpen] = useState(false);
 
-  const iconSize = 35;
+	const navigate =useNavigate()
+	const iconSize = 35;
 
-  const goToPage = (PageUrl: string) => {
-    if (PageUrl === "/") {
-      history.back();
-      return;
-    }
-    window.location.replace(PageUrl);
-  };
+	const goToPage = (PageUrl: string) => {
+		navigate(PageUrl)
+	};
 
-  const dockItems: DockItemData[] = [
-    {
-      icon: <FaHome size={iconSize} />,
-      label: t("home"),
-      onClick: () => {
-        goToPage("/");
-      },
-    },
-    {
-      icon: <FaCode size={iconSize} />,
-      label: t("projects"),
-      onClick: () => {
-        // goToSection("")
-      },
-    },
-    {
-      icon: <IoMdSettings size={iconSize} />,
-      label: t("settings"),
-      onClick: () => {
-        setIsSettingsMenuOpen(!isSettingsMenuOpen);
-      },
-    },
-  ];
+	const dockItems: DockItemData[] = [
+		{
+			icon: <FaHome size={iconSize} />,
+			label: t("home"),
+			onClick: () => {
+				goToPage("/");
+			},
+		},
+		{
+			icon: <FaCode size={iconSize} />,
+			label: t("projects"),
+			onClick: () => {
+				// goToSection("")
+			},
+		},
+		{
+			icon: <IoMdSettings size={iconSize} />,
+			label: t("settings"),
+			onClick: () => {
+				setIsSettingsMenuOpen(!isSettingsMenuOpen);
+			},
+		},
+	];
 
-  const [theme, setTheme] = useState(
-    localStorage.getItem("data-theme") || "dark"
-  );
+	const [theme, setTheme] = useState(
+		localStorage.getItem("data-theme") || "dark"
+	);
 
-  useEffect(() => {
-    localStorage.setItem("data-theme", theme);
-    document.documentElement.setAttribute("data-theme", theme);
-  }, [theme]);
+	useEffect(() => {
+		localStorage.setItem("data-theme", theme);
+		document.documentElement.setAttribute("data-theme", theme);
+	}, [theme]);
 
-  const handleLanguage = () => {
-    if (i18n.language === "ar") {
-      i18n.changeLanguage("en");
-    } else {
-      i18n.changeLanguage("ar");
-    }
-  };
+	const handleLanguage = () => {
+		if (i18n.language === "ar") {
+			i18n.changeLanguage("en");
+		} else {
+			i18n.changeLanguage("ar");
+		}
+	};
 
-  const handleChangeTheme = () => {
-    if (theme === "dark") {
-      setTheme("light");
-    } else {
-      setTheme("dark");
-    }
-  };
-  return (
-    <div>
-      {" "}
-      <div className="w-full sm:w-[40rem] m-auto p-5">
-        {isSettingsMenuOpen && (
-          <SettingsMenu
-            theme={theme}
-            handleChangeTheme={handleChangeTheme}
-            handleLanguage={handleLanguage}
-            onClose={() => setIsSettingsMenuOpen(false)}
-          />
-        )}
+	const handleChangeTheme = () => {
+		if (theme === "dark") {
+			setTheme("light");
+		} else {
+			setTheme("dark");
+		}
+	};
+	return (
+		<div>
+			{" "}
+			<div className="w-full sm:w-[40rem] m-auto p-5">
+				{isSettingsMenuOpen && (
+					<SettingsMenu
+						theme={theme}
+						handleChangeTheme={handleChangeTheme}
+						handleLanguage={handleLanguage}
+						onClose={() => setIsSettingsMenuOpen(false)}
+					/>
+				)}
 
-        <div id="display" className="min-h-[100dvh] w-full ">
-          <Projects></Projects>
-          <Footer></Footer>
-          <div
-            className={`fixed bottom-5 left-1/2 z-40 flex -translate-x-1/2 flex-col items-center`}
-          >
-            <Dock items={dockItems} theme={theme}></Dock>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				<div id="display" className="min-h-[100dvh] w-full ">
+					<Projects></Projects>
+					<Footer></Footer>
+					<div
+						className={`fixed bottom-5 left-1/2 z-40 flex -translate-x-1/2 flex-col items-center`}
+					>
+						<Dock items={dockItems} theme={theme}></Dock>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
